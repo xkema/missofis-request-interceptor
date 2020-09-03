47993f317677cce7c953eafb2edade69e6312341
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-09-03 20:14:29 +0300

    Created sample options object
    - Added trash icon
    - Added monospace font stack

--- extension/core/options.js
+++ extension/core/options.js
@@ -3,10 +3,25 @@
  * context: "options.html"
  */
 
+// {
+//   "redirects": [
+//     {
+//       "from": "some.domain.com/from.js",
+//       "to": "localhost:3000/to.js",
+//       "enabled": true
+//     }
+//   ],
+//   "matches": [
+//     {
+//       "search": "some.cdn.com/images",
+//       "enabled": true
+//     }
+//   ]
+// }
+
 import {getNamespace} from './get-namespace.js';
 import {logger} from './logger.js';
 
 window.browser = getNamespace();
 
 logger('options.js');
-
