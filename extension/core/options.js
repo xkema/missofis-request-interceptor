f001008ac5d60c88f3a7bb041e786517126828b8
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-09-03 12:07:08 +0300

    Combined normalize and base styles into theme.css
    - Added an options.js fole

---
+++ extension/core/options.js
@@ -0,0 +1,12 @@
+/**
+ * @module
+ * context: "options.html"
+ */
+
+import {getNamespace} from './get-namespace.js';
+import {logger} from './logger.js';
+
+window.browser = getNamespace();
+
+logger('options.js');
+
