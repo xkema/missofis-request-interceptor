f15ec515858cf093b9b6c1b9e2d5b7ee18bdde1a
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-09-13 21:34:53 +0300

    Renamed .temp dir to temp withoud dot

--- bin/build.js
+++ bin/build.js
@@ -6,11 +6,11 @@ const fs = require('fs');
 const generateBrowserSpecificDirectories = require('./generate-browser-specific-directories.js');
 const targetBrowsers = require('./target-browsers.js');
 
-fs.rmdirSync(path.resolve(__dirname, '..', 'dist/.temp'), { recursive: true });
+fs.rmdirSync(path.resolve(__dirname, '..', 'dist/temp'), { recursive: true });
 console.clear();
 
 const extensionDir = path.resolve(__dirname, '..', 'extension');
-const tempDir = path.resolve(__dirname, '..', 'dist/.temp');
+const tempDir = path.resolve(__dirname, '..', 'dist/temp');
 
 console.log('\r\n--------------------\r\n');
 
