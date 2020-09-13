c457b7c46c26db7b2df18693c96326a4d9bfc04a
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-09-13 22:49:12 +0300

    Added a file watcher to copy changed files to temp folder on development

--- bin/build.js
+++ bin/build.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 
 // const packageJSON = require('../package.json');
 
-const generateBrowserSpecificDirectories = require('./generate-browser-specific-directories.js');
 const targetBrowsers = require('./target-browsers.js');
+const generateBrowserSpecificDirectories = require('./generate-browser-specific-directories.js');
+const customizeExtensionFile = require('./customize-extension-file.js');
+const customizeManifestJSON = require('./customize-manifest-json.js');
 
 fs.rmdirSync(path.resolve(__dirname, '..', 'dist/temp'), { recursive: true });
 console.clear();
@@ -19,4 +21,35 @@ targetBrowsers.forEach((targetBrowser) => {
   const browserDir = path.join(tempDir, targetBrowser.name);
   generateBrowserSpecificDirectories(extensionDir, browserDir, targetBrowser);
   console.log('\r\n--------------------\r\n');
+});
+
+// watch extension files and copy target on changes
+// @see https://nodejs.org/docs/latest-v12.x/api/fs.html#fs_filename_argument
+let hasActiveFileChange = false;
+fs.watch(extensionDir, { recursive: true }, (eventType, filename) => {
+  if (filename) {
+    if(!hasActiveFileChange) {
+      // throttle changes
+      hasActiveFileChange = true;
+      const timerId = global.setTimeout(() => {
+        hasActiveFileChange = false;
+        global.clearTimeout(timerId);
+      }, 1000);
+      // copy changed file      
+      targetBrowsers.forEach((targetBrowser) => {
+        const srcFilePath = path.join(extensionDir, filename);
+        const destFilePath = path.join(tempDir, targetBrowser.name, filename);
+        const customization = customizeExtensionFile(path.basename(filename), targetBrowser);
+        if(typeof customization === 'string') {
+          fs.writeFileSync(destFilePath, customization);
+          console.log(`copied with customization => "${srcFilePath}"`);
+        } else if(customization === true) {
+          fs.copyFileSync(srcFilePath, destFilePath);
+          console.log(`copied as is => "${srcFilePath}"`);
+        } else {
+          console.log(`skipped => "${srcFilePath}"`);
+        }
+      });
+    }
+  }
 });
