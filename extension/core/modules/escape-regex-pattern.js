cb4fbaf3deebbc1fe5480f7e46624cc7475dddf2
Kemal Yılmaz <kemal.yilmaz@tutanota.com>
2020-09-14 20:55:09 +0300

    Fixed linter error on escape regex pattern module

--- extension/core/modules/escape-regex-pattern.js
+++ extension/core/modules/escape-regex-pattern.js
@@ -8,8 +8,6 @@
  * @param {string} patternRaw - Raw text from user inputs; matches or redirections arrays
  * @returns {string} Escaped string ready to be used on regex constructors
  */
-const escapeRegexPattern = (patternRaw) => {
-  return patternRaw.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
-};
+const escapeRegexPattern = (patternRaw) => patternRaw.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
 
 export { escapeRegexPattern };
